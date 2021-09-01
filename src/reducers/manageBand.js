6453894c26e360deb026026364cd8cdfe16c3f16
dewey <hammersdrichard@gmail.com>
2021-09-01 10:37:12 -0700

    commit tree ce5d058fb198b619f9baccafc37bc7b131792205

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,15 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
-  return state
-};
+export default function manageBand(
+  state = {
+    bands: [],
+  },
+  action
+) {
+  switch (action.type) {
+    case "ADD_BAND":
+      console.log(action.payload);
+      return { bands: [...state.bands, { name: action.payload }] };
+
+    default:
+      return state;
+  }
+}
